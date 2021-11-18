89595dbc87f25c86df0e47fe88fc17d52d3bc450
Swathi04 <swathianandram@gmail.com>
2021-11-17 20:05:39 -0500

    component

--- web-cmpe256/src/components/Home.vue
+++ web-cmpe256/src/components/Home.vue
@@ -6,37 +6,37 @@
     </div>
     <div class="row">
       <div class="col-md-4">
-        <div class="input-group">
-          <select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
-            <option selected>Choose...</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-          </select>
-        </div>
+        <Dropdown :optionslist="source"/>
       </div>
       <div class="col-md-4">
-        <div class="input-group">
-          <select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
-            <option selected>Choose...</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-          </select>
-        </div>
+        <Dropdown :optionslist="authors"/>
       </div>
       <div class="col-md-4">
-
+        <div id="date-picker-example" class="md-form md-outline input-with-post-icon datepicker">
+        <input placeholder="Select date" type="text" id="example" class="form-control">
+        <label for="example">Try me...</label>
+        <i class="fas fa-calendar input-prefix" tabindex=0></i>
+      </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Dropdown from './common/Dropdown.vue'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  components: {
+    Dropdown
+  },
+  data () {
+    return {
+      source: ['CNBC', 'New York Times'],
+      authors: ['Swathi', 'Yash', 'Aryan']
+    }
   }
 }
 </script>
--- web-cmpe256/src/main.js
+++ web-cmpe256/src/main.js
@@ -2,4 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'mdbootstrap/css/mdb.min.css'
 createApp(App).mount('#app')
